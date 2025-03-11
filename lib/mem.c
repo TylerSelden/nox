a59021ed2f2ff65d765ff0d146c4811b1bca7803
TylerSelden <tyler.selden@salesianum.org>
2025-03-11 11:20:54 -0400

    small

--- lib/mem.c
+++ lib/mem.c
@@ -5,25 +5,25 @@
 #include <lib/panic.h>
 
 void mem_init(multiboot_info_t *mbi) {
-  if (!(mbi->flags & 0x41)) {
-    panic("Essential memory information is not available.");
+  if (!(mbi->flags & 0x41)) panic("Essential memory information is not available.");
+
+
+  memory_map_t *mmap = (memory_map_t*) mbi->mmap_addr;
+  for (uint32_t mmap_size = mbi->mmap_length;
+      mmap_size > 0;
+      mmap_size -= mmap->size + sizeof(uint32_t),
+      mmap = (memory_map_t*)((uint32_t) mmap + mmap->size + sizeof(uint32_t))) {
+
+    // for each mmap entry
+    
+    printf("Start: 0x%x%x, end: 0x%x%x, size: 0x%x%x, type: %d",
+       mmap->base_addr_high, mmap->base_addr_low,
+       mmap->base_addr_high + mmap->length_high, mmap->base_addr_low + mmap->length_low,
+       mmap->length_high, mmap->length_low,
+       mmap->type);
+
+
   }
 
-  uint32_t mmap_size = mbi->mmap_length;
-  memory_map_t *mmap = (memory_map_t *)mbi->mmap_addr;
-
-  printf("\nMemory map:\n mem_lower: 0x%x\n mem_upper: 0x%x\n", mbi->mem_lower, mbi->mem_upper);
-  printf("Kernel memory:\n lower: %x\n upper: %x\n", &kernel_start, &kernel_end);
-
-  // Loop through the memory map entries
-  while (mmap_size > 0) {
-    printf("Addr: 0x%x%x; Length: 0x%x%x; Type: %x",
-               mmap->base_addr_high, mmap->base_addr_low,
-               mmap->length_high, mmap->length_low,
-               mmap->type);
-
-    mmap_size -= mmap->size + sizeof(uint32_t);
-    mmap = (memory_map_t *)((uint32_t)mmap + mmap->size + sizeof(uint32_t));
-  }
+  printf("Kernel:\n start: %x, end: %x", &kernel_start, &kernel_end);
 }
-
