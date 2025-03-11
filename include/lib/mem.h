5ebe6f837527140ecfedcc516152f7e3b0c90195
TylerSelden <tyler.selden@salesianum.org>
2025-03-11 15:22:00 -0400

    groot

--- include/lib/mem.h
+++ include/lib/mem.h
@@ -3,6 +3,11 @@
 
 #include <multiboot.h>
 
+typedef struct {
+  uint64_t start;
+  uint64_t end;
+} usable_range_t;
+
 void mem_init(multiboot_info_t *mbi);
 
 #endif
--- lib/mem.c
+++ lib/mem.c
@@ -4,19 +4,15 @@
 #include <multiboot.h>
 #include <lib/panic.h>
 
-typedef struct {
-  uint64_t start;
-  uint64_t end;
-} usable_range_t;
 
 void mem_init(multiboot_info_t *mbi) {
   if (!(mbi->flags & 0x41)) panic("Essential memory information is not available.");
 
-  memory_map_t *mmap = (memory_map_t*) mbi->mmap_addr;
+  multiboot_memory_map_t *mmap = (multiboot_memory_map_t*) mbi->mmap_addr;
   uint32_t mmap_size = mbi->mmap_length;
 
   usable_range_t ranges[32];
-  int range_count = 0;
+  uint32_t range_count = 0;
 
   uint64_t kernel_start = (uint64_t)(uint32_t)&_kernel_start;
   uint64_t kernel_end = (uint64_t)(uint32_t)&_kernel_end;
@@ -24,12 +20,12 @@ void mem_init(multiboot_info_t *mbi) {
   // process all entries, merging where necessary
   for (; mmap_size > 0;
   mmap_size -= mmap->size + sizeof(uint32_t),
-  mmap = (memory_map_t*)((uint32_t) mmap + mmap->size + sizeof(uint32_t))) {
+  mmap = (multiboot_memory_map_t*)((uint32_t) mmap + mmap->size + sizeof(uint32_t))) {
 
     // skip reserved reigons
     if (mmap->type != 1) continue;
-    uint64_t start = ((uint64_t)mmap->base_addr_high << 32) | mmap->base_addr_low;
-    uint64_t end = start + (((uint64_t)mmap->length_high << 32) | mmap->length_low);
+    uint64_t start = mmap->addr;
+    uint64_t end = mmap->addr + mmap->len;
     if (end <= start) continue;
 
     // split for kernel if necessary
@@ -42,8 +38,7 @@ void mem_init(multiboot_info_t *mbi) {
   }
 
   printf("Usable Memory Ranges:");
-  for (int i = 0; i < range_count; i++) {
+  for (uint32_t i = 0; i < range_count; i++) {
     printf("Start: 0x%x, End: 0x%x", (uint32_t)ranges[i].start, (uint32_t)ranges[i].end);
   }
 }
-
