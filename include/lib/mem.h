4063252e0c1da617012b311f79f0551090705983
TylerSelden <tyler.selden@salesianum.org>
2025-03-11 20:56:54 -0400

    it really SHOULD work

--- include/lib/mem.h
+++ include/lib/mem.h
@@ -5,11 +5,6 @@
 
 #define MAX_MMAP_ENTRIES 64
 
-typedef struct {
-  uint64_t start;
-  uint64_t end;
-} usable_range_t;
-
 void sort_mmap_entries(multiboot_memory_map_t *entries[], uint32_t count);
 void mem_init(multiboot_info_t *mbi);
 
--- lib/mem.c
+++ lib/mem.c
@@ -4,7 +4,21 @@
 #include <multiboot.h>
 #include <lib/panic.h>
 
-void sort_mmap_entries(multiboot_memory_map_t *entries[], uint32_t count) {
+// this can't be >255 (nor should it be)
+#define MAX_ENTRIES 64
+
+typedef struct {
+  uint64_t start;
+  uint64_t end;
+} mem_range_t;
+
+multiboot_memory_map_t *entries[MAX_ENTRIES];
+uint8_t entry_count = 0;
+mem_range_t ranges[32];
+uint8_t range_count = 0;
+
+
+static void sort_entries(multiboot_memory_map_t *entries[], uint32_t count) {
   for (uint32_t i = 0; i < count - 1; i++) {
     for (uint32_t j = 0; j < count - i - 1; j++) {
       if (entries[j]->addr > entries[j + 1]->addr) {
@@ -16,32 +30,24 @@ void sort_mmap_entries(multiboot_memory_map_t *entries[], uint32_t count) {
   }
 }
 
-void mem_init(multiboot_info_t *mbi) {
-  if (!(mbi->flags & 0x41)) panic("Essential memory information is not available.");
-
-  multiboot_memory_map_t *mmap_ptr = (multiboot_memory_map_t*) mbi->mmap_addr;
+static void extract_entries(multiboot_info_t *mbi) {
+  multiboot_memory_map_t *mmap = (multiboot_memory_map_t*) mbi->mmap_addr;
   uint32_t mmap_size = mbi->mmap_length;
-  multiboot_memory_map_t *mmap_entries[MAX_MMAP_ENTRIES];
-  uint32_t entry_count = 0;
 
-  while (mmap_size > 0 && entry_count < MAX_MMAP_ENTRIES) {
-    mmap_entries[entry_count++] = mmap_ptr;
-    uint32_t entry_size = mmap_ptr->size + sizeof(uint32_t);
-    mmap_size -= entry_size;
-    mmap_ptr = (multiboot_memory_map_t*)((uint32_t)mmap_ptr + entry_size);
+  while (mmap_size > 0 && entry_count < MAX_ENTRIES) {
+    entries[entry_count++] = mmap;
+    uint32_t size = mmap->size + sizeof(uint32_t);
+    mmap_size -= size;
+    mmap = (multiboot_memory_map_t*)((uint32_t) mmap + size);
   }
+}
 
-  sort_mmap_entries(mmap_entries, entry_count);
-
-  // sorting is done, process the entries
-  usable_range_t ranges[32];
-  uint32_t range_count = 0;
-
+static void extract_usable() {
   uint64_t kernel_start = (uint64_t)(uint32_t)&_kernel_start;
   uint64_t kernel_end  = (uint64_t)(uint32_t)&_kernel_end;
 
   for (uint32_t i = 0; i < entry_count; i++) {
-    multiboot_memory_map_t *mmap = mmap_entries[i];
+    multiboot_memory_map_t *mmap = entries[i];
 
     // skip reserved regions.
     if (mmap->type != 1) continue;
@@ -51,15 +57,24 @@ void mem_init(multiboot_info_t *mbi) {
 
     // check for kernel overlaps
     if (start < kernel_end && end > kernel_start) {
-      if (start < kernel_start) ranges[range_count++] = (usable_range_t){start, kernel_start};
-      if (end > kernel_end) ranges[range_count++] = (usable_range_t){kernel_end, end};
+      if (start < kernel_start) ranges[range_count++] = (mem_range_t){start, kernel_start};
+      if (end > kernel_end) ranges[range_count++] = (mem_range_t){kernel_end, end};
     } else {
-      ranges[range_count++] = (usable_range_t){start, end};
+      ranges[range_count++] = (mem_range_t){start, end};
     }
   }
+}
+
+void mem_init(multiboot_info_t *mbi) {
+  if (!(mbi->flags & 0x41)) panic("Essential memory information is not available.");
+
+  extract_entries(mbi);
+  sort_entries(entries, entry_count);
+  extract_usable(); 
+
 
   uintptr_t size = 0;
-  printf("Usable Memory Ranges:");
+  printf("Ussable Memory Ranges:");
   for (uint32_t i = 0; i < range_count; i++) {
     size += ranges[i].end - ranges[i].start;
     printf("Start: 0x%x, End: 0x%x, Size: %dMB", (uint32_t)ranges[i].start, (uint32_t)ranges[i].end, (uint32_t)(ranges[i].end - ranges[i].start) / 1000000);
